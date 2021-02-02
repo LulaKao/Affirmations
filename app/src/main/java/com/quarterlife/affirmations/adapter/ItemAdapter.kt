482fe490b6dd4b3973694048f72c263a571c4f76
LulaKao <windandsprings@gmail.com>
2021-02-02 16:41:37 +0800

    set item title in OnBindViewHolder

--- app/src/main/java/com/quarterlife/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/quarterlife/affirmations/adapter/ItemAdapter.kt
@@ -8,15 +8,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.quarterlife.affirmations.databinding.ListItemBinding
 import com.quarterlife.affirmations.model.Affirmation
 
+/**
+ * Adapter for the [RecyclerView] in MainActivity. Displays [Affirmation] data object.
+ */
 class ItemAdapter(
     private val context : Context,
     private val dataset : List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     private lateinit var binding: ListItemBinding // add this for view binding
 
+    /**
+     * Provide a reference to the views for each data item.
+     * Complex data items may need more than one view per item,
+     * and you provide access to all the views for a data item in a view holder.
+     * Each data item is just an Affirmation object.
+     */
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view){
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         binding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false) // add this for view binding
         return ItemViewHolder(binding.root) // add this for view binding
@@ -25,6 +37,6 @@ class ItemAdapter(
     override fun getItemCount() = dataset.size
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-//        binding.itemTitle
+        binding.itemTitle.text = context.resources.getString(dataset[position].stringResourceId) // set item title
     }
 }
